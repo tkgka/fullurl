889e121e64397c2afca5837df079effd9e94d4ed
tkgka <ksh17171@gmail.com>
2021-11-16 22:14:23 +0900

    ./vue.config.js 추가

--- src/assets/axios.ts
+++ src/assets/axios.ts
@@ -1,11 +1,10 @@
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
-import axios from 'axios';
+import axios from "axios";
 const client = axios.create({
-  baseURL: 'http://serve.oozoo.site:8080/',
+  baseURL: "http://serve.oozoo.site:8080/",
 });
-
-
+/* eslint-disable */
 export default function getTrack(TINYURL: string) {
   return client.get(`/?url=${TINYURL}`);
 }
