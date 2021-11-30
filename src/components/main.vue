cfb78a3194fd8af90f7d51fa0b9a2d88e0c24df4
tkgka <ksh17171@gmail.com>
2021-11-30 22:26:01 +0900

    ./ 이미지 default 값 false 로 수정

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,6 +2,9 @@
   <div>
     <div class="form-control">
       <label class="label">
+        <th @click="myFilter()" class="initial">
+          <span class="label-text">image_{{ image_show }}</span>
+        </th>
         <span class="label-text">URL을 입력하세요</span>
       </label>
       <div class="flex space-x-2">
@@ -18,7 +21,7 @@
           <span class="text-lg font-bold"> </span>
         </div>
       </div>
-      <img :src="imageLink" alt="" />
+      <img :src="imageLink" alt="" v-if="image_show" />
     </div>
   </div>
 </template>
@@ -38,9 +41,14 @@ export default Vue.extend({
       return_value: "",
       imageLink: "",
       tinyurl: "",
+      image_show: false,
     };
   },
   methods: {
+    myFilter: function () {
+      this.image_show = !this.image_show;
+      this.tinyurl != "" ? this.toggleimg(`https://${this.tinyurl}`) : null;
+    },
     async verify_URL(input_data) {
       input_data.match(reg.pattern)
         ? (this.valid = true)
@@ -78,7 +86,9 @@ export default Vue.extend({
           fullurl(this.tinyurl).then((val) => {
             val.data !== "" && val.data !== "error"
               ? ((this.return_value = String(val.data)),
-                this.toggleimg(`https://${this.tinyurl}`))
+                this.image_show === true
+                  ? this.toggleimg(`https://${this.tinyurl}`)
+                  : null)
               : alert("URL 정보 확인에 실패 했습니다.");
           });
         }
